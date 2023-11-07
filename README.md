ba96e7b1bd39d52a57dca16499cd353dfe41d55e
kinling9 <vmin6810@gmail.com>
2023-11-07 20:02:38 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # my_config.lua
+
+## other settings
+1. font: Meslo LGL Nerd Font
+2. delete CTRL + V in wsl short keys
+
+## requirements
+- clangd
+- ripgrep
+- fd
+- lazygit *turn off in nvim*
+- python neovim *not used, just for health check* 
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -70,11 +70,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     -- vim.keymap.set('n', '<space>f', function()
     --   vim.lsp.buf.format { async = true }
     -- end, opts)
   end,
 })
 
-map('n', '<leader>g', "<cmd>lua _lazygit_toggle()<CR>", opts)
+-- map('n', '<leader>g', "<cmd>lua _lazygit_toggle()<CR>", opts)
