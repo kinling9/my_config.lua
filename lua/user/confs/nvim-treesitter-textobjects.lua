053611fa285f89cc803f397b7e4943dfabc2a5ac
kinling9 <vmin6810@gmail.com>
2026-04-09 10:10:02 +0800

    update ignore and fix config

--- lua/user/confs/nvim-treesitter-textobjects.lua
+++ lua/user/confs/nvim-treesitter-textobjects.lua
@@ -1,24 +1,26 @@
 return function()
-  local ok, textobjects = pcall(require, "nvim-treesitter-textobjects")
+  local ok, configs = pcall(require, "nvim-treesitter.configs")
   if not ok then
     vim.schedule(function()
-      vim.notify("nvim-treesitter-textobjects is not available", vim.log.levels.WARN)
+      vim.notify("nvim-treesitter.configs is not available", vim.log.levels.WARN)
     end)
     return
   end
 
-  textobjects.setup({
-    select = {
-      lookahead = true,
-      selection_modes = {
-        ["@parameter.outer"] = "v",
-        ["@function.outer"] = "V",
-        ["@class.outer"] = "<c-v>",
+  configs.setup({
+    textobjects = {
+      select = {
+        lookahead = true,
+        selection_modes = {
+          ["@parameter.outer"] = "v",
+          ["@function.outer"] = "V",
+          ["@class.outer"] = "<c-v>",
+        },
+        include_surrounding_whitespace = true,
       },
-      include_surrounding_whitespace = true,
-    },
-    move = {
-      set_jumps = true,
+      move = {
+        set_jumps = true,
+      },
     },
   })
 end
